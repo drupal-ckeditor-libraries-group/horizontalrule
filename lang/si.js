6eae3ab249c9f025d1aaf6ae89aea8952d4bcee3
Wiktor Walc <w.walc@cksource.com>
2013-06-10 13:22:00 +0200

    Updated translations, added Sinhala language.

--- lang/si.js
+++ lang/si.js
@@ -2,6 +2,6 @@
 Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'horizontalrule', 'ja', {
-	toolbar: '水平線'
+CKEDITOR.plugins.setLang( 'horizontalrule', 'si', {
+	toolbar: 'තිරස් රේඛාවක් ඇතුලත් කරන්න'
 });
